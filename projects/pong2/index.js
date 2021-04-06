f5dc7df871a39688c90f1462f295ab717c35fdc3
Tonyroux <tonyrouxrito@yahoo.com>
2021-04-06 15:00:37 -0500

    better collisions and challenge mode

--- projects/pong2/index.js
+++ projects/pong2/index.js
@@ -28,7 +28,12 @@ function runProgram(){
       x: x,
       y: y,
       width: $(id).width(),
-      height: $(id).width(),
+      originalW: $(id).width(),
+      height: $(id).height(),
+      left: x,
+      right: x + $(id).width(),
+      top: y,
+      bottom: y + $(id).height(),
       speedX: velX,
       speedY: velY,
       id: id
@@ -45,14 +50,18 @@ function runProgram(){
   //game vars
   var score1 = 0;
   var score2 = 0;
+  var winningScore = prompt("How many points should be required to win?")
+  checkWinCount();
   var oneWin = "Player One wins!";
   var twoWin = "Player Two wins!";
+  var challenge = false;
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyD);                           // change 'eventType' to the type of event you want to handle
   $(document).on('keyup', handleKeyU);
   $("#oldSchool").on('click', applyFilter);
+  $("#challenge").on('click', challengeMode);
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -98,47 +107,73 @@ function runProgram(){
     obj.x += obj.speedX;
     $(obj.id).css("top", obj.y); // draw paddleLeft in the new location, positionY pixels away from the "top"
     $(obj.id).css("left", obj.x);
+
+    redrawBounds(obj);
+  }
+
+  function redrawBounds(obj) {
+    obj.left = obj.x;
+    obj.right = obj.x + obj.width;
+    obj.top = obj.y;
+    obj.bottom = obj.y + obj.height;
   }
 
   function paddleBounds(obj) {
-    if (obj.x < 0) {
+    if (obj.left < 0) {
       $(obj.id).css("left", 0)
       obj.x = 0;
     }
-    if (obj.x > (boardWidth - obj.width)) {
+    if (obj.right > boardWidth) {
       $(obj.id).css("left", (boardWidth - obj.width))
       obj.x = (boardWidth - obj.width);
     }
   }
+
   function ballBounds(obj) {
     //wall collisions
-    if (obj.x < 0) {
+    if (obj.left < 0) {
       $(obj.id).css("left", 0)
       obj.x = 0;
       obj.speedX = -obj.speedX;
     }
-    if (obj.x > (boardWidth - obj.width)) {
+    if (obj.right > boardWidth) {
       $(obj.id).css("left", (boardWidth - obj.width))
       obj.x = (boardWidth - obj.width);
       obj.speedX = -obj.speedX;
     }
 
     //paddle collisions
-    if (obj.y <= (topPaddle.y + 20) && obj.x > topPaddle.x && obj.x < (topPaddle.x + topPaddle.width) && obj.y >= (topPaddle.y - 20) && obj.speedY < 0) {
+    if (obj.top <= topPaddle.bottom && obj.left > topPaddle.left && obj.right < topPaddle.right && obj.top >= topPaddle.top && obj.speedY < 0) {
       obj.speedY = -obj.speedY;
-    }
-    if (obj.y >= (bottomPaddle.y - 20) && obj.x > bottomPaddle.x && obj.x < (bottomPaddle.x + bottomPaddle.width) && obj.y <= (bottomPaddle.y + 20) && obj.speedY > 0) {
-      obj.speedY = -obj.speedY;
-    }
-    //paddle sides
-    if (obj.x == topPaddle.x + topPaddle.width || obj.x == topPaddle.x - obj.width) {
-      if (obj.y < topPaddle.y + 20 && obj.y > topPaddle.y - 20) {
-        obj.speedX = -obj.speedX;
+      if (challenge == true) {
+        changePaddle(topPaddle);
       }
     }
-    if (obj.x == bottomPaddle.x + bottomPaddle.width || obj.x == bottomPaddle.x - obj.width) {
-      if (obj.y < bottomPaddle.y - 20 && obj.y > bottomPaddle.y + 20) {
-        obj.speedX = -obj.speedX;
+    if (obj.bottom >= bottomPaddle.top && obj.left > bottomPaddle.left && obj.right < bottomPaddle.right && obj.bottom <= bottomPaddle.bottom && obj.speedY > 0) {
+      obj.speedY = -obj.speedY;
+      if (challenge == true) {
+        changePaddle(bottomPaddle);
+      }
+    }
+    //paddle sides
+    if (obj.left <= topPaddle.right && obj.left >= topPaddle.right - 15) {
+      if (obj.top < topPaddle.bottom && obj.bottom > topPaddle.top) {
+        obj.speedX = 3;
+      }
+    }
+    if (obj.right >= topPaddle.left && obj.right <= topPaddle.left + 15) {
+      if (obj.top < topPaddle.bottom && obj.bottom > topPaddle.top) {
+        obj.speedX = -3;
+      }
+    }
+    if (obj.left <= bottomPaddle.right && obj.left >= bottomPaddle.right - 15) {
+      if (obj.bottom < bottomPaddle.bottom && obj.top > bottomPaddle.top) {
+        obj.speedX = 3;
+      }
+    }
+    if (obj.right >= bottomPaddle.left && obj.right <= bottomPaddle.left + 15) {
+      if (obj.bottom < bottomPaddle.bottom && obj.top > bottomPaddle.top) {
+        obj.speedX = -3;
       }
     }
 
@@ -147,17 +182,19 @@ function runProgram(){
       score2 ++;
       ball = makeItem(boardWidth / 2, boardHeight / 2, (Math.random() > 0.5 ? -3 : 3), 1, "#ball");
       updateScore(score2, "#score2");
+      changePaddle(topPaddle, "reset");
     }
     if (obj.y >= boardHeight - obj.height) {
       score1 ++;
       ball = makeItem(boardWidth / 2, boardHeight / 2, (Math.random() > 0.5 ? -3 : 3), -1, "#ball");
       updateScore(score1, "#score1");
+      changePaddle(bottomPaddle, "reset");
     }
   }
 
   function updateScore (newScore, tag) {
     $(tag).text(newScore);
-    if (newScore == 3) {
+    if (newScore == parseInt(winningScore)) {
       handleVictory(tag)
     }
   }
@@ -173,7 +210,6 @@ function runProgram(){
     $("#winner").css("top", (boardHeight / 2) - ($("#winner").height() / 2));
     $("#playAgain").css("left", (boardWidth / 2) - ($("#playAgain").width() / 2) - 15);
     $("#playAgain").css("top", (boardHeight * 0.66) - ($("#playAgain").height() / 2));
-    $("#ball").css("left", boardWidth * 4);
     setInterval(function() {
       endGame();
     }, 10);
@@ -182,6 +218,28 @@ function runProgram(){
   function applyFilter() {
     $(".game").css("z-index") == 3 ? $(".game").css("z-index", -1) : $(".game").css("z-index", 3);
   }
+
+  function challengeMode() {
+    challenge = true
+  }
+
+  function changePaddle(obj, action) {
+    if (action == "reset") {
+      $(obj.id).css("width", obj.originalW);
+      obj.width = obj.originalW;
+    } else {
+      $(obj.id).css("width", (Math.random() * (2 * $(obj.id).width())));
+      obj.width = $(obj.id).width();
+    }
+  }
+
+  function checkWinCount() {
+    if (Number.isInteger(parseInt(winningScore)) == false) {
+      winningScore = prompt("Must be an integer");
+      checkWinCount();
+    }
+    else{}
+  }
   
   function endGame() { 
     // stop the interval timer
@@ -189,6 +247,8 @@ function runProgram(){
 
     // turn off event handlers
     $(document).off();
+
+    $("#ball").css("left", boardWidth * 4);
   }
   
 }
