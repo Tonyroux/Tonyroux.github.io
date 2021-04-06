828a2933ae2b6bb2131fee07c89ff4a4abfd9183
Tonyroux <tonyrouxrito@yahoo.com>
2021-04-06 02:52:10 -0500

    pong finish

--- projects/pong2/index.js
+++ projects/pong2/index.js
@@ -37,24 +37,22 @@ function runProgram(){
   }
 
   var bottomPaddle = makeItem(((boardWidth / 2) - ($("#bottomPad").width() / 2)), boardHeight - 50, 0, 0, "#bottomPad");
-  //bottomPaddle.user = prompt("Player One:");
 
   var topPaddle = makeItem(((boardWidth / 2) - ($("#topPad").width() / 2)), 20, 0, 0, "#topPad");
-  //topPaddle.user = prompt("Player Two:");
 
   var ball = makeItem(boardWidth / 2, boardHeight / 2, (Math.random() > 0.5 ? -3 : 3), (Math.random() > 0.5 ? -1 : 1), "#ball");
 
   //game vars
   var score1 = 0;
   var score2 = 0;
-  var oneWin = bottomPaddle.user + " wins!";
-  var twoWin = topPaddle.user + " wins!";
-  var winner = "#winner";
+  var oneWin = "Player One wins!";
+  var twoWin = "Player Two wins!";
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyD);                           // change 'eventType' to the type of event you want to handle
   $(document).on('keyup', handleKeyU);
+  $("#oldSchool").on('click', applyFilter);
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -126,18 +124,63 @@ function runProgram(){
     }
 
     //paddle collisions
-    if (obj.y <= (topPaddle.y + 20) && obj.x > topPaddle.x && obj.x < (topPaddle.x + topPaddle.width) && obj.y >= (topPaddle.y - 20)) {
+    if (obj.y <= (topPaddle.y + 20) && obj.x > topPaddle.x && obj.x < (topPaddle.x + topPaddle.width) && obj.y >= (topPaddle.y - 20) && obj.speedY < 0) {
       obj.speedY = -obj.speedY;
     }
-    if (obj.y >= (bottomPaddle.y - 20) && obj.x > bottomPaddle.x && obj.x < (bottomPaddle.x + bottomPaddle.width) && obj.y <= (bottomPaddle.y + 20)) {
+    if (obj.y >= (bottomPaddle.y - 20) && obj.x > bottomPaddle.x && obj.x < (bottomPaddle.x + bottomPaddle.width) && obj.y <= (bottomPaddle.y + 20) && obj.speedY > 0) {
       obj.speedY = -obj.speedY;
     }
-    if ((obj.x == topPaddle.x + topPaddle.width || obj.x == topPaddle.x) && obj.y < topPaddle.y + 20 && obj.y > topPaddle.y - 20) {
-      obj.speedX = -obj.speedX;
+    //paddle sides
+    if (obj.x == topPaddle.x + topPaddle.width || obj.x == topPaddle.x - obj.width) {
+      if (obj.y < topPaddle.y + 20 && obj.y > topPaddle.y - 20) {
+        obj.speedX = -obj.speedX;
+      }
     }
-    if ((obj.x == topPaddle.x + bottomPaddle.width || obj.x == bottomPaddle.x) && obj.y > bottomPaddle.y - 20 && obj.y < bottomPaddle.y + 20) {
-      obj.speedX = -obj.speedX;
+    if (obj.x == bottomPaddle.x + bottomPaddle.width || obj.x == bottomPaddle.x - obj.width) {
+      if (obj.y < bottomPaddle.y - 20 && obj.y > bottomPaddle.y + 20) {
+        obj.speedX = -obj.speedX;
+      }
     }
+
+    //victory triggers
+    if (obj.y <= 0) {
+      score2 ++;
+      ball = makeItem(boardWidth / 2, boardHeight / 2, (Math.random() > 0.5 ? -3 : 3), 1, "#ball");
+      updateScore(score2, "#score2");
+    }
+    if (obj.y >= boardHeight - obj.height) {
+      score1 ++;
+      ball = makeItem(boardWidth / 2, boardHeight / 2, (Math.random() > 0.5 ? -3 : 3), -1, "#ball");
+      updateScore(score1, "#score1");
+    }
+  }
+
+  function updateScore (newScore, tag) {
+    $(tag).text(newScore);
+    if (newScore == 3) {
+      handleVictory(tag)
+    }
+  }
+
+  function handleVictory (scoreTag) {
+    if (scoreTag == "#score1") {
+      $("#winner").text(oneWin);
+    }
+    else {
+      $("#winner").text(twoWin);
+    }
+    $("#winner").css("left", (boardWidth / 2) - ($("#winner").width() / 2));
+    $("#winner").css("top", (boardHeight / 2) - ($("#winner").height() / 2));
+    $("#playAgain").css("left", (boardWidth / 2) - ($("#playAgain").width() / 2) - 15);
+    $("#playAgain").css("top", (boardHeight * 0.66) - ($("#playAgain").height() / 2));
+    $("#ball").css("left", boardWidth * 4);
+    setInterval(function() {
+      endGame();
+    }, 10);
+  }
+
+  function applyFilter() {
+    $(".game").css("z-index") == 3 ? $(".game").css("z-index", -1) : $(".game").css("z-index", 3);
   }
   
   function endGame() { 
