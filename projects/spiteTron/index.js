d3b14c54cc2807bdd5b3c208351448bc46dc30ff
tonyroux <tonyrouxrito@yahoo.com>
2021-04-08 11:00:43 -0500

    tron almost

--- projects/spiteTron/index.js
+++ projects/spiteTron/index.js
@@ -13,15 +13,19 @@ function runProgram(){
 
   var KEY = {
     "ENTER": 13,
-    "LEFT": 65,
-    "RIGHT": 68,
-    "UP": 87,
-    "DOWN": 83,
+    "A": 65,
+    "D": 68,
+    "W": 87,
+    "S": 83,
+    "LEFT": 37,
+    "RIGHT": 39,
+    "UP": 38,
+    "DOWN": 40,
   }
   
   // Game Item Objects
   
-  function makeItem(x, y, velX, velY, id) {
+  function makeItem(x, y, velX, velY, id, color) {
     var itemInstance = {
       x: x * 20,
       y: y * 20,
@@ -36,26 +40,30 @@ function runProgram(){
       bottom: (y * 20) + $(id).height(),
       speedX: velX,
       speedY: velY,
-      id: id
+      id: id,
+      passId: id.substring(1, id.length),
+      canLeft: velX > 0 ? false : true,
+      canRight: velX < 0 ? false : true,
+      canUp: velY > 0 ? false : true,
+      canDown: velY < 0 ? false : true, 
+      color: color,
     };
     return itemInstance;
   }
 
-  var snakeHead = makeItem(5, 6, 20, 0, "#snakeHead");
+  var bike1 = makeItem(3, 3, 20, 0, "#bike1", "rgb(242, 225, 94)");
 
-  var snakeTotal = [snakeHead];
+  var player1 = [bike1];
 
-  for (var i = 0; i < 3; i++) {
-    $('<div id="snakeBody' + i + '" class = "snake"></div>').insertAfter("#snakeHead");
-    snakeTotal.push(makeItem(5, (snakeTotal[i].y / 20) + 1, 0, 0, "#snakeBody" + i))
-  }
+  var playerLength1 = player1.length;
 
-  var snakeLength = snakeTotal.length;
+  var bike2 = makeItem(12, 9, -20, 0, "#bike2", "rgb(103, 198, 249)");
 
-  var canLeft = false;
-  var canRight = true;
-  var canUp = true;
-  var canDown = true;
+  var player2 = [bike2];
+
+  var playerLength2 = player2.length;
+
+  var pause = false;
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
@@ -70,49 +78,83 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    snakeLength = snakeTotal.length;
-    redrawGameItem(snakeTotal);
-    checkWalls(snakeTotal);
-    checkSelf(snakeTotal);
-    //checkFruit(snakeTotal, fruit);
+    redrawGameItem(player1);
+    redrawGameItem(player2);
+
+    checkWalls(player1);
+    checkWalls(player2);
+
+    checkSelf(player1);
+    checkSelf(player2);
   }
   
   /* 
   Called in response to events.
   */
   function handleKeyDown(event) {
+    if(!pause)
+    if (event.which == KEY.W) {
+      console.log("up");
+      if (bike1.speedY == 0 && bike1.canUp) {
+        bike1.speedY = -20;
+        bike1.speedX = 0;
+      }
+    }
+    if (event.which == KEY.S) {
+      console.log("down");
+      if (bike1.speedY == 0 && bike1.canDown) {
+        bike1.speedY = 20;
+        bike1.speedX = 0;
+      }
+    }
+    if (event.which == KEY.D) {
+      console.log("right");
+      if (bike1.speedX == 0 && bike1.canRight) {
+        bike1.speedY = 0;
+        bike1.speedX = 20;
+      }
+    }
+    if (event.which == KEY.A) {
+      console.log("left");
+      if (bike1.speedX == 0 && bike1.canLeft) {
+        bike1.speedY = 0;
+        bike1.speedX = -20;
+      }
+    }
+
     if (event.which == KEY.UP) {
       console.log("up");
-      if (snakeHead.speedY == 0 && canUp) {
-        snakeHead.speedY = -20;
-        snakeHead.speedX = 0;
+      if (bike2.speedY == 0 && bike2.canUp) {
+        bike2.speedY = -20;
+        bike2.speedX = 0;
       }
     }
     if (event.which == KEY.DOWN) {
       console.log("down");
-      if (snakeHead.speedY == 0 && canDown) {
-        snakeHead.speedY = 20;
-        snakeHead.speedX = 0;
+      if (bike2.speedY == 0 && bike2.canDown) {
+        bike2.speedY = 20;
+        bike2.speedX = 0;
       }
     }
     if (event.which == KEY.RIGHT) {
       console.log("right");
-      if (snakeHead.speedX == 0 && canRight) {
-        snakeHead.speedY = 0;
-        snakeHead.speedX = 20;
+      if (bike2.speedX == 0 && bike2.canRight) {
+        bike2.speedY = 0;
+        bike2.speedX = 20;
       }
     }
     if (event.which == KEY.LEFT) {
       console.log("left");
-      if (snakeHead.speedX == 0 && canLeft) {
-        snakeHead.speedY = 0;
-        snakeHead.speedX = -20;
+      if (bike2.speedX == 0 && bike2.canLeft) {
+        bike2.speedY = 0;
+        bike2.speedX = -20;
       }
     }
+
     if (event.which == KEY.ENTER) {
       console.log("enter");
-      snakeHead.speedY = 0;
-      snakeHead.speedX = 0;
+      bike1.speedY = 0;
+      bike1.speedX = 0;
     }
   }
 
@@ -121,25 +163,19 @@ function runProgram(){
   ////////////////////////////////////////////////////////////////////////////////
 
   
-  function endGame() {
+  /*function endGame() {
     // stop the interval timer
     clearInterval(interval);
 
     // turn off event handlers
     $(document).off();
-  }
-
-  /*function repositionFruit() {
-    fruit = makeItem(Math.floor(Math.random() * 20), Math.floor(Math.random() * 20), 0, 0, "#fruit");
-    $(fruit.id).css("left", fruit.x);
-    $(fruit.id).css("top", fruit.y);
   }*/
 
-  function redrawGameItem(snake) {
-    for (var i = 0; i < snakeLength; i++) {
-      let obj = snake[i]
+  function redrawGameItem(player) {
+    for (var i = 0; i < player.length; i++) {
+      let obj = player[i]
 
-      if (obj == snake[0]) {
+      if (obj == player[0]) {
           obj.prevX = obj.x
           obj.x += obj.speedX;
 
@@ -149,59 +185,62 @@ function runProgram(){
 
       else {
         obj.prevX = obj.x;
-        obj.x = snake[i - 1].prevX;
+        obj.x = player[i - 1].prevX;
         obj.prevY = obj.y;
-        obj.y = snake[i - 1].prevY;
+        obj.y = player[i - 1].prevY;
         if ($(obj.id).css("z-index") == -2) {
           $(obj.id).css("z-index", 1)
         }
       }
       
-      //console.log("newPosX: " + newPosX + ", newPosY: " + newPosY + ", velY: " + velocityY + ", velX: " + velocityX);
       $(obj.id).css("left", obj.x);
       $(obj.id).css("top", obj.y);
     }
-  }
 
-  function checkWalls(snake) {
-    let obj = snake[0];
-
-    if (obj.x < 0 || obj.x > $("#board").width() - 20 || obj.y < 0 || obj.y > $("#board").height() - 20) {
-      endGame();
+    if (player.length < 61) {
+      addTrail(player);
     }
   }
 
-  function checkSelf(snake) {
-    let head = snake[0];
+  function checkWalls(player) {
+    let obj = player[0];
 
-    for (var i = 1; i < snakeLength; i++) {
-      if (head.x == snake[i].x && head.y == snake[i].y) {
-        endGame();
+    if (obj.x < 0 || obj.x > $("#board").width() - 20 || obj.y < 0 || obj.y > $("#board").height() - 20) {
+      //killPlayer(player);
+    }
+  }
+
+  function checkSelf(player) {
+    let bike = player[0];
+
+    for (var i = 1; i < player.length; i++) {
+      if (bike.x == player[i].x && bike.y == player[i].y) {
+        //killPlayer(player);
       }
     }
 
-    head.x > head.prevX ? canLeft = false : canLeft = true;
-    head.x < head.prevX ? canRight = false : canRight = true;
-    head.y > head.prevY ? canUp = false : canUp = true;
-    head.y < head.prevY ? canDown = false : canDown = true;
-  }
-  
-  /*function checkFruit(snake, fruit) {
-    let head = snake[0];
-
-    if (head.x == fruit.x && head.y == fruit.y) {
-      addSegment();
-      repositionFruit();
-      updateScore();
-    }
-  }*/
-
-  function addSegment () {
-    $('<div id="snakeBody' + (snakeTotal.length - 1) + '" class = "snake" style= "z-index: -2;"></div>').insertAfter("#snakeHead");
-    snakeTotal.push(makeItem(snakeTotal[snakeTotal.length - 1].prevX / 20, snakeTotal[snakeTotal.length - 1].prevY / 20, 0, 0, "#snakeBody" + (snakeTotal.length - 1)))
+    bike.x > bike.prevX ? bike.canLeft = false : bike.canLeft = true;
+    bike.x < bike.prevX ? bike.canRight = false : bike.canRight = true;
+    bike.y > bike.prevY ? bike.canUp = false : bike.canUp = true;
+    bike.y < bike.prevY ? bike.canDown = false : bike.canDown = true;
   }
 
-  function updateScore(){
-    $("#score").text(snakeTotal.length - 4);
+  function addTrail (player) {
+    $('<div id="' + player[0].passId + 'trail' + (player.length - 1) + '" class = "trail" style= "z-index: -2; background-color:' + player[0].color + ';"></div>').insertAfter(player[0].id);
+    player.push(makeItem(player[player.length - 1].prevX / 20, player[player.length - 1].prevY / 20, 0, 0, "#" + player[0].passId + 'trail' + (player.length - 1)))
+  }
+
+  function killPlayer(player) {
+    pause = true;
+    player1.speedX = 0;
+    player1.speedY = 0;
+    player2.speedX = 0;
+    player2.speedY = 0;
+
+    updateScore(player);
+  }
+
+  function updateScore(player){
+    $("#score").text(player.length - 4);
   }
 }
